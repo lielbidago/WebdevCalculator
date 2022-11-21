68ee8fdb86471d280e5be0fd011b62ec9ad5afb1
lielbidago <liloplev@gmail.com>
2022-11-21 14:39:18 +0200

    editing application.js

--- js/application.js
+++ js/application.js
@@ -1,1 +1,12 @@
-
+function change_mode(id){
+     
+    if(id =='mode'){
+    if(document.body.className =='light'){
+        document.body.className ='dark';
+        return;
+    }else{
+        document.body.className ='light';
+        return;
+    }      
+    }
+}
